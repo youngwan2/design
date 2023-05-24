2ade3ebd0da54f31e7f20dd2db97ed9550c1de27
KYW <qodna25@gmail.com>
2023-05-24 13:01:07 +0900

    데일리 뮤직 슬라이드 애니메이션 적용 까지

---
+++ prototype/js/app.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+    $('.da_music').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        autoplya:true,
+        autoplaySpeed:1,
+        arrows:false
+    });
+  }); 
+
+
+//   $(document).ready(function(){
+//     $('.da_music').slick({
+//         slidesToShow:1,
+//         slidesToScroll:1,
+//         autoplya:true,
+//         autoplaySpeed:1,
+//         arrows:false
+//     });
+//   }); 
